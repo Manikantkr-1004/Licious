55a24145d7658edcb0999f89e821ff752de10994
Shashikant kumar <loveuzindagi212@gmail.com.com>
2023-03-29 21:51:05 +0530

    I have added day 2 project file

---
+++ homepage.js
@@ -0,0 +1,174 @@
+const product1 = [
+    {
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/ffd4144f-25a7-6f26-2d90-9a9db0332dda/original/Chicken_Thigh_Boneless_Hero_Shot.jpg",
+        name: "Chicken Thigh Boneless",
+        weight: "450gms",
+        price: 271,
+        date: "Today 8 PM - 10 PM",
+        id: 1
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/44f90ab6-ee35-f696-7713-354e52e6fae5/original/Afghani-Murgh-Seekh-Kebab-uncooked.jpg",
+        name: "Afghani Murgh Seekh Kebab",
+        weight: "4 Pieces",
+        price: 269,
+        date: "Today 8 PM - 10 PM",
+        id: 2
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/4e9da018-2a68-a7bb-4c86-3b73f3432b78/original/WhatsApp_Image_2022-04-13_at_2.16.31_PM.jpeg",
+        name: "Freshwater/ White Prawns/ Jhinga",
+        weight: "250gms",
+        price: 469,
+        date: "Today 8 PM - 10 PM",
+        id: 3
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/1f3264b6-397f-ea66-286e-52f450666eb0/original/Peppery-Chicken-Salami-1.jpg?format=webp",
+        name: "Chunky Shawarma Chicken Spread",
+        weight: "1 Pieces",
+        price: 175,
+        date: "Today 8 PM - 10 PM",
+        id: 4
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/7b633c75-c203-6c03-5234-9fd75467133a/original/Chicken-Boneless---Tikka-Cut--Hero-Shot_(1).jpg",
+        name: "Chicken Boneless Cubes",
+        weight: "450gms",
+        price: 335,
+        date: "Today 8 PM - 10 PM",
+        id: 5
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/d5ba4d2e-0347-046e-26e9-c22d23dbb030/original/Chicken-Breast-Boneless-(3-4-Pieces)-Hero-Shot.jpg?format=webp",
+        name: "Chicken Breast Boneless",
+        weight: "450gms",
+        price: 445,
+        date: "Today 8 PM - 10 PM",
+        id: 6
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/44f90ab6-ee35-f696-7713-354e52e6fae5/original/Afghani-Murgh-Seekh-Kebab-uncooked.jpg?format=webp",
+        name: "Afghani Murgh Seekh Kebab",
+        weight: "250gms",
+        price: 226,
+        date: "Today 8 PM - 10 PM",
+        id: 7
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/ca5adea0-fcd4-9787-416a-7b95b3a670b4/original/ChickenLC.jpg?format=webp",
+        name: "Chicken Curry Cut - Large Pieces",
+        weight: "500gms",
+        price: 512,
+        date: "Today 8 PM - 10 PM",
+        id: 8
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/feedc98d-42e1-0e0a-7da0-0072fa84977c/original/p2_tile_images_6th_folder-09_(1).jpg?format=webp",
+        name: "Rohu (Rui) Medium - Bengali Cut, No Head",
+        weight: "100gms",
+        price: 441,
+        date: "Today 8 PM - 10 PM",
+        id: 9
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/be46f1fa-1aa8-81ea-7a47-11260815a274/original/Chicken_MiniBites_Boneless_Hero_Shot.jpg?format=webp",
+        name: "Chicken Boneless - Mini Bites",
+        weight: "300gms",
+        price: 229,
+        date: "Today 8 PM - 10 PM",
+        id: 10
+    },{
+        img: "https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_4cyjya5ry5e/3/prod_display_image/1634636505.7759--2021-10-1915:11:45--1818?format=webp",
+        name: "Chunky Butter Chicken Spread",
+        weight: "1 Pieces",
+        price: 199,
+        date: "Today 8 PM - 10 PM",
+        id: 11
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/a9d5332f-3683-4321-5e3e-577e59d1b45b/original/Chunky-Herby-Tomato-Chicken-Spread_(1).jpg?format=webp",
+        name: "Chunky Herby Tomato Chicken Spread",
+        weight: "1 Pieces",
+        price: 199,
+        date: "Today 8 PM - 10 PM",
+        id: 12
+    },{
+        img: "https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_4q4jya5s665/3/prod_display_image/1578400556.0345--2020-01-0718:05:56--458?format=webp",
+        name: "Chunky Honey-Mustard Chicken Spread",
+        weight: "1 Pieces",
+        price: 179,
+        date: "Today 8 PM - 10 PM",
+        id: 13
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/dbeb3262-60f0-eb31-9bf9-82c68a9f0e20/original/Chunky-Shawarma-Chicken-Spread_(7).jpg?format=webp",
+        name: "Chunky Shawarma Chicken Spread",
+        weight: "1 Pieces",
+        price: 219,
+        date: "Today 8 PM - 10 PM",
+        id: 14
+    },{
+        img: "https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/fee95638-21fd-1706-4569-ed42430fd716/original/Sriracha-chicken-spreadsTIle-image.jpg?format=webp",
+        name: "Chunky Sriracha Chicken Spread",
+        weight: "1 Pieces",
+        price: 206,
+        date: "Today 8 PM - 10 PM",
+        id: 15
+    }
+]
+
+let container= document.getElementById("container")
+let filter=  document.getElementById("filter");
+let search = document.getElementById("searchbar");
+
+filter.addEventListener("change", function(){
+    if(filter.value===""){
+        Display(product1);
+    }
+    if(filter.value==="lth"){
+        let dofilter= [...product1];
+        dofilter.sort(function(a,b){
+            return a.price - b.price;
+        })
+        Display(dofilter);
+    }
+    if(filter.value==="htl"){
+        let dofilter= [...product1];
+        dofilter.sort(function(a,b){
+            return b.price - a.price;
+        })
+        Display(dofilter);
+    }
+})
+
+search.addEventListener("input",function(){
+    let filtered= product1.filter(function(element){
+        if(element.name.toUpperCase().includes(search.value.toUpperCase())===true){
+            return true;
+        }else{
+            return false;
+        }
+    })
+    Display(filtered);
+})
+
+
+
+Display(product1);
+
+function Display(data){
+    container.innerHTML= "";
+    for(let i=0; i<data.length; i++){
+        let box= document.createElement("div");
+        let cart= document.createElement("button")
+        let images= document.createElement("img");
+        let names= document.createElement("h4");
+        let weights= document.createElement("p");
+        let prices= document.createElement("h4");
+        let deliveries= document.createElement("p");
+
+        images.src= data[i].img;
+        names.innerText= data[i].name;
+        weights.innerText = data[i].weight;
+        prices.innerText= "₹"+data[i].price;
+        deliveries.innerText= data[i].date;
+        cart.innerText = "+ Add To Cart"
+
+        cart.addEventListener("click",function(){
+            alert("First Login in Website")
+        })
+
+        box.append(images, cart, names, weights, prices, deliveries);
+        container.append(box);
+    }
+    };
