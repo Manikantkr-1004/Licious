e879d2e891b196fc92b9e603c32560b7da02a2cf
Shashikant kumar <loveuzindagi212@gmail.com.com>
2023-03-31 22:27:52 +0530

    I have added day4 files and updated past day's file

---
+++ cart.js
@@ -0,0 +1,98 @@
+let container = document.getElementById("container");
+let length = document.getElementById("length");
+let totalprice = document.getElementById("totalprice")
+let pay= document.getElementById("pay");
+let show= document.getElementById("show");
+let logout= document.getElementById("logout");
+
+
+let cartdata= JSON.parse(localStorage.getItem("cartdata")) || [];
+let paymentdata = JSON.parse(localStorage.getItem("paymentdata")) || [];
+
+logout.addEventListener("click", function(){
+    alert("Logout Successful!!");
+    setTimeout(() => {
+        window.location.href = "./homepage.html";
+    }, 2000);
+})
+
+length.innerText = cartdata.length;
+
+Display(cartdata);
+
+function Display(data){
+    container.innerHTML = "";
+
+    data.forEach((element,index) => {
+        
+        length.innerText = data.length;
+        let box= document.createElement("div");
+        let images= document.createElement("img");
+        let names= document.createElement("h4");
+        let weights= document.createElement("p");
+        let prices= document.createElement("h4");
+        let deliveries= document.createElement("p");
+        let increase= document.createElement("button");
+        let quantity= document.createElement("span");
+        let decrease= document.createElement("button");
+        let remove= document.createElement("button");
+
+        images.src= element.img;
+        names.innerText= element.name;
+        weights.innerText = element.weight;
+        prices.innerText= "₹"+element.price;
+        deliveries.innerText= element.date;
+        increase.innerText = "+";
+        quantity.innerText = element.quantity;
+        decrease.innerText = "-";
+        remove.innerText = "Remove";
+
+        remove.addEventListener("click",function(){
+            data.splice(index,1);
+            localStorage.setItem("cartdata", JSON.stringify(data));
+            Display(data);
+
+            length.innerText = data.length;
+        })
+
+        increase.addEventListener("click", function(){
+            element.quantity++;
+            localStorage.setItem("cartdata", JSON.stringify(data));
+            Display(data);
+        })
+
+        decrease.addEventListener("click", function(){
+            if(element.quantity>1){
+                element.quantity--;
+                localStorage.setItem("cartdata", JSON.stringify(data));
+                Display(data);
+            }
+        })
+
+        totalprice.innerText = calculate(data);
+
+        function calculate(data){
+
+            let sum=0;
+            for(let i=0; i<cartdata.length; i++){
+                sum+= cartdata[i].quantity*cartdata[i].price;
+            }
+            return sum;
+        }
+
+        box.append(images, names, weights, prices, increase, quantity, decrease,deliveries, remove);
+        container.append(box);
+
+        pay.addEventListener("click", function(){
+
+            show.innerHTML = '<img style="width:13%;" src="https://gifdb.com/images/high/animated-transparent-background-check-mark-lb1gygckicpca0fb.gif">';
+            paymentdata.push({...element, totalcost: totalprice.innerText});
+            localStorage.setItem("paymentdata", JSON.stringify(paymentdata));
+            setTimeout(() => {
+                window.location.href = "./payment.html"
+            }, 2000);
+        })
+
+
+    });
+}
